83c5c59ae4d88f514c0feead374ec989882ab085
Michael Simpson <msimpson@intellicentrics.com>
2022-03-03 17:51:16 -0600

    auth interceptor

--- libs/node-tests/src/lib/interceptors/auth.interceptor.ts
+++ libs/node-tests/src/lib/interceptors/auth.interceptor.ts
@@ -3,17 +3,24 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ServiceService } from '../services/service.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor() {}
-
+  constructor(private authService: ServiceService) { }
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log("INTERCEPT!");
-    return next.handle(request);
+    console.log("Intercept!");
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if(err.status === 401) {
+          console.log("Please log in!");
+        }
+        throw (err.message);
+        }));
   }
 }
